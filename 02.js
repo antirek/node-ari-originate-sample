9252f2e6dddf8dee2f9f1af07a7be5c0ec9e5a4d
Sergey Dmitriev <serge.dmitriev@gmail.com>
2017-10-08 14:42:12 +0700

    use bridge for two channels

--- 02.js
+++ 02.js
@@ -1,39 +1,69 @@
 const client = require('ari-client');
+const uuid = require('uuid/v4');
 
 const url = "http://localhost:8088";
 const [username, password] = ["test", "test"];
 
-async function main()
-{
-  try {
-    const ari = await client.connect(url, username, password);
+let ari, channel1_Id, channel2_Id;
 
-    ari.start('app');
 
+const createChannel = function (onStasisStart) {
     const channel = ari.Channel();
-    channel.on('StasisStart', (event, channel) => {
-      console.log('stasis start', event);
-
-      channel.originate({
-        endpoint: 'SIP/102',
-        app: 'app',
-        appArgs: 'hello, world  '
-      });
-    });
+    
+    channel.on('StasisStart', onStasisStart);
 
     channel.on('ChannelDtmfReceived', (event, channel) => {
       console.log('channel dtmf received', event);
     });
 
-    channel.on('StasisStop', (event, channel) => {
-      ari.stop();
-      console.log('mojn mojn');
-    });
+    return channel;
+}
 
-    await channel.originate({
+const onStasisStart1 = (event, channel) => {
+  console.log('stasis start 1', event);
+  channel1_Id = event.channel.id;
+
+  const channel2 = createChannel(onStasisStart2);
+
+  channel2.originate({
+    endpoint: 'SIP/102',
+    app: 'app',
+  }, (err, channel) => {
+    console.log('originate channel2', channel);
+  });
+}
+
+const onStasisStart2 = (event, channel) => {
+  console.log('stasis start 2', event);
+
+  channel2_Id = event.channel.id;
+  
+  console.log('bridge ids', channel1_Id, channel2_Id);
+
+  const bridge = ari.Bridge();
+
+  bridge.create()
+      .then((bridge) => {
+          console.log('bridge', bridge);
+          bridge.addChannel({channel: channel1_Id});
+          bridge.addChannel({channel: channel2_Id});
+      })
+ 
+}
+
+
+async function main()
+{
+  try {
+    ari = await client.connect(url, username, password);
+
+    ari.start('app');
+
+    let channel1 = createChannel(onStasisStart1);
+  
+    await channel1.originate({
       endpoint: 'SIP/101',
       app: 'app',
-      appArgs: 'hello,world'
     });
   } catch (e) {
     console.log('err:', err);
